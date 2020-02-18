b76353fab8a3cdb7a64ea312699f1c25c99c447c
tommaclean <tom.j.maclean@gmail.com>
2020-02-18 16:00:00 -0500

    adjusted user params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :fav_meetups
+    has_many :meetups, through: :meetups
+
+
+    # def fav_meetups
+    #     FavMeetup.all.select do |fav_meetup|
+    #         fav_meetup.user === current_user
+    #     end
+    # end
+
+    # def meetups 
+    #     fav_meetups.map do |fav_meetup|
+    #         fav_meetup.meetup
+    #     end
+    # end
 end
+
+
+
+# users         fav_meetups               meetups
+# 1, bob          1, 1                        1, 711
+# 2, tom          1, 2                        2, barnes&noble
+#                 2, 2
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  post '/login', to: 'auth#create'
-  post '/signup', to: 'users#create'
-  get '/profile', to:  'users#profile'
-  resources :fav_meetups
-  resources :meetups
-  resources :users
+  # namespace :api do 
+  #   namespace :v1 do 
+      post '/login', to: 'auth#create'
+      post '/signup', to: 'users#create'
+      get '/profile', to:  'users#profile'
+      resources :fav_meetups#, only: [:create, :new, :delete]
+      resources :meetups
+      resources :users
+  #   end 
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
