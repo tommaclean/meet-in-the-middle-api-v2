6d859c843d8837ce9391f579bc730f2fc54ffb93
tommaclean <tom.j.maclean@gmail.com>
2020-12-16 22:09:57 -0500

    added create meetup

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -3,4 +3,18 @@ class MeetupsController < ApplicationController
         @meetups = Meetup.all
         render json: @meetups
     end
+
+    def create
+        meetup = Meetup.new(meetup_params)
+        if meetup.save
+          render json: meetup
+        else
+          render json: { errors: meetup.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    def meetup_params
+    params.permit(:creator, :location)
+    end
 end
