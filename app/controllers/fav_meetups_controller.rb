876283723f40a162a9c628374e1177378a27882a
tommaclean <tom.j.maclean@gmail.com>
2020-12-24 16:22:24 -0500

    added fav_meetup controller logic

--- app/controllers/fav_meetups_controller.rb
+++ app/controllers/fav_meetups_controller.rb
@@ -1,2 +1,20 @@
 class FavMeetupsController < ApplicationController
+    def index
+        @fav_meetups = FavMeetup.all
+        render json: @fav_meetups
+    end
+
+    def create
+        meetup = FavMeetup.new(fav_meetup_params)
+        if fav_meetup.save
+          render json: fav_meetup
+        else
+          render json: { errors: fav_meetup.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    def fav_meetup_params
+    params.permit(:meetup, :user)
+    end
 end
